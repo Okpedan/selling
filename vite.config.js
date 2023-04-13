b55f542ff3a33cc7372f3cca6cde467653f3565b
Daniel Okpe <88175754+Okpedan@users.noreply.github.com>
2023-04-13 04:15:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import terser from '@rollup/plugin-terser';
-//import { dependencies } from './package.json';
 
-function renderChunks(deps) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
